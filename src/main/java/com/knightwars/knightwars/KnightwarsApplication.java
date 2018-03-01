2301cd7d2831c6b0dd35432971a25fddb85aa6d6
dimirhouse <dimirhouse@gmail.com>
2018-03-01 12:10:00 +0100

    first commit

---
+++ src/main/java/com/knightwars/knightwars/KnightwarsApplication.java
@@ -0,0 +1,12 @@
+package com.knightwars.knightwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnightwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnightwarsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/knightwars/knightwars/KnightwarsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.knightwars.knightwars;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnightwarsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
